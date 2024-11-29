8381494d66beca64d251ead5e0f869739a36530c
Martin Kronberger <e12202316@student.tuwien.ac.at>
2024-11-29 12:22:46 +0100

    flake update

--- Calculations/Python/flake.nix
+++ Calculations/Python/flake.nix
@@ -4,10 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
-  outputs = { self, nixpks }:
+  outputs = { self, nixpkgs }:
   let
     system = "x86_64-linux";
-    pkgs = nixpks.LegacyPackages.${system};
+    pkgs = nixpkgs.legacyPackages.${system};
   in
   {
     devShells.${system}.default =
